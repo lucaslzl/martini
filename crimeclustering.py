3a630f64a1a44a9b218bd5572d7017dd00db16f0
Lucas Zanco Ladeira <lznladeira@gmail.com>
2019-02-12 12:38:06 -0200

    Plot

--- crimeclustering.py
+++ crimeclustering.py
@@ -300,13 +300,11 @@ class FixedWindowClustering:
 		return interval
 
 	def metric_close_crimes(self, clusters, start, end):
-
 		return 0
 
 	def clusterize(self, month_crimes, clustering):
 
-		result_max = []
-		result_close = []
+		result_max, result_close = [], []
 	
 		for i in range(0, 24, self.size):
 			window_crime = self.get_window(month_crimes, i, i+self.size)
@@ -319,7 +317,148 @@ class FixedWindowClustering:
 
 
 class TimeMinutesClustering:
-	pass
+
+	def __init__(self):
+		self.u = Util()
+
+	def make_gauss(self, N=1, sig=1, mu=0):
+		return lambda xt: N/(sig * (2*np.pi)**.5) * np.e ** (-(xt-mu)**2/(1000 * sig**2))
+
+	def calculate_difference(self, hour, minute, ref_hour, ref_minute):
+
+		time = hour*60 + minute
+		ref_time = ref_hour*60 + ref_minute
+
+		xt = time - ref_time
+
+		score = self.make_gauss()(xt)
+
+		return float('%.5f' % (score))
+
+
+	def calculate_score(self, crimes_filtered):
+
+		window_scores = []
+
+		for hour in range(0, 24):
+
+			for minute in range(0, 60, 10):
+
+				state_score = []
+				for index, row in crimes_filtered.iterrows():
+					state_score.append(self.calculate_difference(row['hour'], row['minute'], hour, minute))
+
+				window_scores.append(np.sum(state_score))
+
+		return window_scores
+
+	def identify_window(self, window_scores, peaks):
+
+		iterpeaks = iter(peaks)
+		next(iterpeaks)
+		last_peak = peaks[0]
+
+		apeaks = []
+
+		for peak in iterpeaks:
+
+			apeaks.append(last_peak + np.argmin(window_scores[last_peak:peak]))
+			last_peak = peak
+
+		return [0] + apeaks
+
+	def get_window(self, start, end, crimes_filtered):
+
+		start_hour = start * 10 // 60
+		start_minute = start * 10 % 60
+
+		end_hour = end * 10 // 60
+		end_minute = end * 10 % 60
+
+		# Filter the closed interval
+		crimes_opened = crimes_filtered.query('hour > {0} & hour < {1}'.format(start_hour, end_hour))
+
+		# Filter the opened interval
+		crimes_closed_low = crimes_filtered.query('hour == {0} & minute >= {1}'.format(start_hour, start_minute))
+		crimes_closed_high = crimes_filtered.query('hour == {0} & minute < {1}'.format(end_hour, end_minute))
+
+		return pd.concat([crimes_opened, crimes_closed_low, crimes_closed_high])
+
+	def convert_to_minutes(self, hour, minutes):
+		return hour * 60 + minutes
+
+	def metric_max_interval(self, clusters):
+
+		interval = 0
+		indx_cluster = clusters['cluster'].max()
+
+		if not np.isnan(indx_cluster):
+
+			for i in range(0, indx_cluster):
+
+				cluster_interval = 0
+				last_datetime = None
+
+				crime_clusters = clusters.query('cluster == ' + str(i)).sort_values(by=['minute', 'hour'])
+
+				for index, row in crime_clusters.iterrows():
+
+					if cluster_interval == 0:
+						last_datetime = self.convert_to_minutes(row['hour'], row['minute'])
+						cluster_interval = 1
+
+					else:
+						diff = self.convert_to_minutes(row['hour'], row['minute']) - last_datetime
+
+						if diff > cluster_interval:
+							cluster_interval = diff
+
+						last_datetime = self.convert_to_minutes(row['hour'], row['minute'])
+
+				if cluster_interval > interval:
+					interval = cluster_interval
+
+		return interval
+	
+	def metric_close_crimes(self, clusters, start, end):
+		return 0
+
+	def clusterize(self, month_crimes, clustering):
+
+		result_max, result_close = [0], [0]
+		crimes = month_crimes.groupby('type').all().index
+
+		for crime in crimes:
+			
+			crimes_filtered = month_crimes.query("type == '%s'" % crime)
+			crimes_filtered = clustering.clusterize(crimes_filtered).query('cluster != -1')
+
+			if not crimes_filtered.empty:
+				
+				window_scores = self.calculate_score(crimes_filtered)
+
+				peaks = find_peaks(window_scores, distance=9)[0]
+
+				if len(peaks) > 0:
+				
+					window = self.identify_window(window_scores, peaks)
+
+					if len(window) > 0:
+
+						iterwindow = iter(window)
+						next(iterwindow)
+						last_window = window[0]
+						for iw in iterwindow:
+							
+							crimes_window = self.get_window(last_window, iw, crimes_filtered)
+
+							cluster_crime = clustering.clusterize(crimes_window).query('cluster != -1')
+							result_max.append(self.metric_max_interval(cluster_crime))
+							result_close.append(self.metric_close_crimes(cluster_crime, last_window, iw))
+
+							last_window = iw
+
+		return np.max(result_max), np.max(result_close)
 
 
 ######################################################################
@@ -329,30 +468,54 @@ class CompareClustering:
 	def __init__(self):
 		self.u = Util()
 
+	def plot_max_metric(self, result_max):
+		
+		fig, ax = plt.subplots()
+
+		x = len(result_max[0])
+
+		for result in result_max:
+			ax.plot(x, result, 'o--')
+
+		labels = []
+		for month in range(1, 13):
+			labels.append(self.u.MONTHS[month])
+			for day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
+				labels.append(day)
+		
+		#plt.xticks(indx)
+		ax.set_xticks(labels, minor=True)
+		#ax.set(xlabel='time (s)', ylabel='voltage (mV)',
+		#       title='About as simple as it gets, folks')
+		ax.grid()
+		plt.show()
+
 	def clusterize(self):
 
 		clustering = Clustering()
 
-		for day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
+		for month in range(1, 13):
 
-			day_crimes = self.u.read_data(day)
+			print('#' + self.u.MONTHS[month])
 
-			print('### ' + day)
+			for day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
 
-			for month in range(1, 13):
+				print('### ' + day)
 
-				print('#' + self.u.MONTHS[month])
+				day_crimes = self.u.read_data(day)
 
 				month_crimes = day_crimes['2018-' + str(month)]
 
 				result_strategy = {'max': [[], [], [], [], [], []], 'close': [[], [], [], [], [], []]}
 
 				for indx, strategy in enumerate([FixedWindowClustering(1), FixedWindowClustering(2), FixedWindowClustering(4), FixedWindowClustering(8), FixedWindowClustering(12),\
-								TimeMinutesClustering()]):
+					TimeMinutesClustering()]):
 
 					maxi, close = strategy.clusterize(month_crimes, clustering)
 					result_strategy['max'][indx].append(maxi)
 					result_strategy['close'][indx].append(close)
+
+		self.plot_max_metric(result_strategy['max'])
 
 
 ######################################################################
