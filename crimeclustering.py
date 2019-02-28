59d240d1357e256eeb55e3ceee623c2282dcfdf4
Lucas <lznladeira@gmail.com>
2019-02-28 16:21:25 -0300

    Clean code and generate new analysis

--- crimeclustering.py
+++ crimeclustering.py
@@ -104,191 +104,6 @@ class Util:
 
 ######################################################################
 
-
-'''
-	Initial code for crime clustering considering minutes interval and gaussian distribution
-'''
-class CrimeClustering:
-
-	def __init__(self):
-		self.u = Util()
-
-	def make_gauss(self, N=1, sig=1, mu=0):
-		return lambda x: N/(sig * (2*np.pi)**.5) * np.e ** (-(x-mu)**2/(410 * sig**2))
-
-	def calculate_difference(self, hour, minute, ref_hour, ref_minute):
-
-		time = hour*60 + minute
-		ref_time = ref_hour*60 + ref_minute
-
-		xt = time - ref_time
-
-		score = self.make_gauss()(xt)
-
-		return float('%.5f' % (score))
-
-	def normalize(self, window_scores):
-
-		maxi = np.amax(window_scores)
-		mini = np.amin(window_scores)
-
-		for indx, w in enumerate(window_scores):
-			window_scores[indx] = (w - mini) / (maxi - mini)
-
-		return window_scores
-
-	def calculate_score(self, crimes_filtered):
-
-		window_scores = []
-
-		for hour in range(0, 24):
-
-			for minute in range(0, 60, 10):
-
-				state_score = []
-				for index, row in crimes_filtered.iterrows():
-					state_score.append(self.calculate_difference(row['hour'], row['minute'], hour, minute))
-
-				window_scores.append(np.sum(state_score))
-
-		return self.normalize(window_scores)
-
-	def plot_to_see(self, window_scores):
-
-		plt.figure(1)
-		plt.subplot(211)
-
-		for w in window_scores:
-			plt.plot(range(0, 144), w, '--')
-			
-		plt.xticks(np.arange(0, 145, 6), np.arange(0, 25))
-		plt.grid(True)
-
-		plt.show()
-
-	def plot_to_save(self, window_scores, name):
-
-		plt.clf()
-		plt.subplot(211)
-
-		plt.plot(range(0, 144), window_scores, '--')
-			
-		plt.xticks(np.arange(0, 145, 6), np.arange(0, 25))
-		#plt.grid(True)
-
-		plt.grid('off', axis='x')
-		plt.grid('on', axis='y')
-
-		plt.savefig('plots/' + str(name) + '.pdf', bbox_inches="tight", format='pdf')
-
-	def identify_window(self, window_scores, peaks):
-
-
-		iterpeaks = iter(peaks)
-		next(iterpeaks)
-		last_peak = peaks[0]
-
-		apeaks = []
-
-		for peak in iterpeaks:
-
-			apeaks.append(last_peak + np.argmin(window_scores[last_peak:peak]))
-			last_peak = peak
-
-		return [0] + apeaks
-
-	def get_window(self, start, end, crimes_filtered):
-
-		start_hour = start * 10 // 60
-		start_minute = start * 10 % 60
-
-		end_hour = end * 10 // 60
-		end_minute = end * 10 % 60
-
-		# Filter the closed interval
-		crimes_opened = crimes_filtered.query('hour > {0} & hour < {1}'.format(start_hour, end_hour))
-
-		# Filter the opened interval
-		crimes_closed_low = crimes_filtered.query('hour == {0} & minute >= {1}'.format(start_hour, start_minute))
-		crimes_closed_high = crimes_filtered.query('hour == {0} & minute < {1}'.format(end_hour, end_minute))
-
-		return pd.concat([crimes_opened, crimes_closed_low, crimes_closed_high])
-
-	# Write data
-
-	'''
-		Save clusters
-	'''
-	def write_clusters(self, clusters, month, day, start, crime):
-
-		if not os.path.exists('clusters'):
-			os.makedirs('clusters')
-
-		output_file = open('clusters/{0}_{1}_{2}_{3}_clusters.txt'.format(self.u.MONTHS[month], str(day), crime, self.format_digits(str(start))), 'w')
-
-		for cluster in clusters:
-			for point in cluster:
-				output_file.write(str(point[0]) + ' ' + str(point[1]) + '; ')
-
-			output_file.write('\n')
-		output_file.close()
-
-	def clusterize(self):
-
-		clustering = Clustering()
-
-		for day in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']:
-
-			day_crimes = self.u.read_data(day)
-
-			print('### ' + day)
-
-			for month in range(1, 13):
-
-				print('#' + self.u.MONTHS[month])
-
-				df_crimes = day_crimes['2018-' + str(month)]
-				crimes = df_crimes.groupby('type').all().index
-
-				windows = []
-
-				for crime in crimes:
-					
-					crimes_filtered = df_crimes.query("type == '%s'" % crime)
-					crimes_filtered = clustering.clusterize(crimes_filtered).query('cluster != -1')
-
-					if not crimes_filtered.empty:
-						
-						window_scores = self.calculate_score(crimes_filtered)
-						windows.append(window_scores)
-
-						#self.plot_to_save(window_scores, crime)
-				
-						peaks = find_peaks(window_scores, distance=9)[0]
-
-						if len(peaks) > 0:
-						
-							window = self.identify_window(window_scores, peaks)
-
-							if len(window) > 0:
-								iterwindow = iter(window)
-								next(iterwindow)
-								last_window = window[0]
-								for iw in iterwindow:
-									
-									crimes_window = self.get_window(last_window, iw, crimes_filtered)
-
-									cluster_crime = clustering.clusterize(crimes_window)
-									clusters = self.u.format_clusters(cluster_crime)
-
-									#self.write_clusters(clusters, month, day, last_window, crime)
-									
-									last_window = iw
-
-		
-######################################################################
-
-
 class FixedWindowClustering:
 
 	def __init__(self, size):
@@ -370,7 +185,7 @@ class TimeMinutesClustering:
 		self.u = Util()
 
 	def make_gauss(self, N=1, sig=1, mu=0):
-		return lambda xt: N/(sig * (2*np.pi)**.5) * np.e ** (-(xt-mu)**2/(1000 * sig**2))
+		return lambda x: N/(sig * (2*np.pi)**.5) * np.e ** (-(x-mu)**2/(410 * sig**2))
 
 	def calculate_difference(self, hour, minute, ref_hour, ref_minute):
 
@@ -490,6 +305,51 @@ class TimeMinutesClustering:
 	def calculate_percentage_max(self, interval, windowsize):
 		return 100 * interval / (windowsize*10)
 
+	'''
+		Save clusters
+	'''
+	def write_clusters(self, clusters, month, day, start, crime):
+
+		if not os.path.exists('clusters'):
+			os.makedirs('clusters')
+
+		output_file = open('clusters/{0}_{1}_{2}_{3}_clusters.txt'.format(self.u.MONTHS[month], str(day), crime, self.format_digits(str(start))), 'w')
+
+		for cluster in clusters:
+			for point in cluster:
+				output_file.write(str(point[0]) + ' ' + str(point[1]) + '; ')
+
+			output_file.write('\n')
+		output_file.close()
+
+	def plot_to_see(self, window_scores):
+
+		plt.figure(1)
+		plt.subplot(211)
+
+		for w in window_scores:
+			plt.plot(range(0, 144), w, '--')
+			
+		plt.xticks(np.arange(0, 145, 6), np.arange(0, 25))
+		plt.grid(True)
+
+		plt.show()
+
+	def plot_to_save(self, window_scores, name):
+
+		plt.clf()
+		plt.subplot(211)
+
+		plt.plot(range(0, 144), window_scores, '--')
+			
+		plt.xticks(np.arange(0, 145, 6), np.arange(0, 25))
+		#plt.grid(True)
+
+		plt.grid('off', axis='x')
+		plt.grid('on', axis='y')
+
+		plt.savefig('plots/' + str(name) + '.pdf', bbox_inches="tight", format='pdf')
+
 	def clusterize(self, month_crimes, clustering):
 
 		result_max = [0]
@@ -557,6 +417,7 @@ class CompareClustering:
 
 	def plot_ecdf(self, result_max):
 
+		plt.clf()
 		for indx, result in enumerate(result_max):
 			ax, _, _ = ecdf(x=result)
 		
@@ -564,6 +425,7 @@ class CompareClustering:
 
 	def plot_max_metric(self, result_max):
 		
+		plt.clf()
 		fig, ax = plt.subplots()
 
 		x = [x for x in range(len(result_max[0]))]
@@ -617,7 +479,7 @@ class CompareClustering:
 					result_strategy[indx].append(maxi)
 
 		self.plot_max_metric(result_strategy)
-		#self.plot_ecdf(result_strategy)
+		self.plot_ecdf(result_strategy)
 
 
 ######################################################################
